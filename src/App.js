0d9fa1b1a6d31ffae1ec19de6c0e1a8aca9544fd
BernadetteCh <b.christiner93@gmail.com>
2022-02-11 20:17:45 +0100

    integrated ToDoList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import ToDoList from "./ToDoList.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <ToDoList />
     </div>
   );
 }
