d322751af43d022698d5d41b9364b80098fb359f
BernadetteCh <b.christiner93@gmail.com>
2022-02-12 18:21:32 +0100

    added comment to the code

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -3,8 +3,8 @@ import Navigation from "./Navigation.js";
 import Output from "./Output.js";
 
 export default function ToDoList() {
-  const [todolist, setTodolist] = useState("");
-  const [list, setList] = useState("");
+  const [todolist, setTodolist] = useState(""); //mit diesem state zeig i das value was beim input eingegeben wird
+  const [list, setList] = useState("");//dieses state sorgt dafür, dass das eingegebene erst erscheint sobald man auf enter drückt 
 
   function handleChange(event) {
     event.preventDefault();
@@ -35,19 +35,3 @@ export default function ToDoList() {
     </div>
   );
 }
-
-/*
-  return (
-    <div className="ToDoList">
-      <h1>To-Do List</h1>
-      <h2>Add Task</h2>
-      <div className="input">
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Add to list" onChange={handleChange}></input>
-          <button>Create</button>
-        </form>
-      </div>
-      <Navigation />
-      <Output output={todo} />
-    </div>
-  );*/
