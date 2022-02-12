2f14e53d7c26415b9958d00b728582137e22f7da
BernadetteCh <b.christiner93@gmail.com>
2022-02-12 18:18:38 +0100

    added functions to the form, created states to display input and integrated Output component

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./Navigation.js";
+import Output from "./Output.js";
 
 export default function ToDoList() {
+  const [todolist, setTodolist] = useState("");
+  const [list, setList] = useState("");
+
+  function handleChange(event) {
+    event.preventDefault();
+  setTodolist(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setList(todolist);
+  }
+
   return (
     <div className="ToDoList">
       <h1>To-Do List</h1>
       <h2>Add Task</h2>
       <div className="input">
-        <form>
-          <input type="text" placeholder="Add to list"></input>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add to list"
+            onChange={handleChange}
+          ></input>
           <button>Create</button>
         </form>
       </div>
       <Navigation />
+      <Output output={list} />
     </div>
   );
 }
+
+/*
+  return (
+    <div className="ToDoList">
+      <h1>To-Do List</h1>
+      <h2>Add Task</h2>
+      <div className="input">
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Add to list" onChange={handleChange}></input>
+          <button>Create</button>
+        </form>
+      </div>
+      <Navigation />
+      <Output output={todo} />
+    </div>
+  );*/
