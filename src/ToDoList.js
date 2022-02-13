5788b1ec6af42b478fd8b99adc4bb1040c57ef70
BernadetteCh <b.christiner93@gmail.com>
2022-02-13 17:51:32 +0100

    added state, created an Array and added comments

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -3,66 +3,64 @@ import Navigation from "./Navigation.js";
 import Output from "./Output.js";
 
 export default function ToDoList() {
-
-const[input, setInput]=useState("");//dieses State sorgt dafür, dass was im input eingegeben wird dann als Todo erscheint
-const[loaded, setLoaded]=useState(null);//dieses State sorgt dafür, dass beim Laden der Seite erstmal nur die Hauptform gezeigt wird, erst 
-                                        //indem ich setLoaded auf true setze in der function handleSubmit, erscheint erst bei Eingabebestätigung die eingegeben Todo. 
-
-
+  const [input, setInput] = useState(""); //dieses State sorgt dafür, dass was im input eingegeben wird dann als Todo erscheint
+  const [todos, setTodos] = useState([]);
+  const [loaded, setLoaded] = useState(null); //dieses State sorgt dafür, dass beim Laden der Seite erstmal nur die Hauptform gezeigt wird, erst
+  //indem ich setLoaded auf true setze in der function handleSubmit, erscheint erst bei Eingabebestätigung die eingegeben Todo.
 
   function handleChange(event) {
     event.preventDefault();
     setInput(event.target.value);
- 
+    console.log(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    setTodos([
+      ...todos,
+      { text: input, completed: false, id: Math.random() * 1000 },
+    ]); //...todos  damit sog i dem PC:wenn ich schon todos habe füge die neue Eingabe einfach hinzu 
     setLoaded(true);
-  
   }
 
-  if(loaded){
-      return (
-        <div className="ToDoList">
-          <h1>To-Do List</h1>
-          <h2>Add Task</h2>
-          <div className="input">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="Add to list"
-                className="input"
-                onChange={handleChange}
-              ></input>
-              <button>Create</button>
-            </form>
-          </div>
-          <Navigation />
-          <Output output={input} />
+  if (loaded) {
+    return (
+      <div className="ToDoList">
+        <h1>To-Do List</h1>
+        <h2>Add Task</h2>
+        <div className="input">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Add to list"
+              className="input"
+              onChange={handleChange}
+            ></input>
+            <button>Create</button>
+          </form>
         </div>
-      );
-
-  }else{
-    return(
-       <div className="ToDoList">
-          <h1>To-Do List</h1>
-          <h2>Add Task</h2>
-          <div className="input">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="Add to list"
-                className="input"
-                onChange={handleChange}
-              ></input>
-              <button>Create</button>
-            </form>
-          </div>
-          <Navigation />
-          </div>
-
-    )
+        <Navigation />
+        <Output output={todos} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="ToDoList">
+        <h1>To-Do List</h1>
+        <h2>Add Task</h2>
+        <div className="input">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Add to list"
+              className="input"
+              onChange={handleChange}
+            ></input>
+            <button>Create</button>
+          </form>
+        </div>
+        <Navigation />
+      </div>
+    );
   }
 }
-
