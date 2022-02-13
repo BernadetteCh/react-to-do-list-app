f7775eb8bbf0fd0ecff2078994dbf85c55b55327
BernadetteCh <b.christiner93@gmail.com>
2022-02-13 15:14:33 +0100

    removed one input

--- src/Output.js
+++ src/Output.js
@@ -2,19 +2,23 @@ import React from "react";
 import "./Output.css";
 
 export default function Output(props){
-    return (
-      <div className="output">
-        <form>
-          <fieldset>
-            <input type="checkbox"  />
-            <label>{props.output}</label>
-            <span>🙉</span>
-           <br />
-            <input type="checkbox" value="Eier kaufen" />
-            <label></label>
-            <span>🙉</span>
-          </fieldset>
-        </form>
-      </div>
-    );
-}
+
+
+
+      return (
+        <div className="output">
+        
+          <form>
+            <fieldset>
+              <input type="checkbox" />
+              <label>{props.output}</label>
+              <span>🙉</span>
+              <br />
+            </fieldset>
+          </form>
+        </div>
+      );
+
+
+  }
+ 
