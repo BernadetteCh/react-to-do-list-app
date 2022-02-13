cd749d99cb0deb82d393411708fa30136fe08e30
BernadetteCh <b.christiner93@gmail.com>
2022-02-13 17:49:41 +0100

    created new component

---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function List(){
+    return (
+      <div>
+        <fieldset>
+          <input type="checkbox" />
+          <label>Milch kaufen</label>
+          <span>🙉</span>
+          <br />
+        </fieldset>
+      </div>
+    );
+}
