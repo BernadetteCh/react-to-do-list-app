1eb4da864a2564f1d2b9043c8a8761c2c76c0be2
BernadetteCh <b.christiner93@gmail.com>
2022-02-15 20:15:43 +0100

    created state to remove todo whenever it gets clicked

--- src/List.js
+++ src/List.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function List(props){
-    return (
-      <div>
-        <fieldset>
-          <input type="checkbox" />
-          <label>{props.list.text}</label>
-          <span>🙉</span>
-          <br />
-        </fieldset>
-      </div>
-    );
+export default function List(props) {
+  const[checked, setChecked]=useState(props.list.text);
+  
+
+  function handleCheck(event) {
+    event.preventDefault();
+    setChecked(false);
+  }
+
+if(checked){
+  return (
+    <div className="list">
+      <fieldset>
+        <input type="checkbox" onClick={handleCheck} />
+        <label>{props.list.text}</label>
+        <span>🙉</span>
+        <br />
+      </fieldset>
+    </div>
+  );
+}else{
+  return(
+    null
+  );
+}
+
 }
