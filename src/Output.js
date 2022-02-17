120ceb5a29a6359b11fcf2a87d1b1cd7b58b4e1f
BernadetteCh <b.christiner93@gmail.com>
2022-02-17 08:56:18 +0100

    added setTodos from the state to the List component

--- src/Output.js
+++ src/Output.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./Output.css";
 import List from "./List.js";
 
-export default function Output(props) {
-  console.log(props.output);
+
+export default function Output({output,setTodos}) {
+ 
 //hier habe ich ein neues component (<List />) erstellt, damit ich durch loopen kann, weil jedes mal wenn 
 //i a Todo eintippe soll diese dann im output erscheinen als eigenes List component
 return (
   <div className="output">
     <form>
-      {props.output.map(function (list, index) {
+      {output.map(function (list) {
         return (
-          <div key={index}>
-            <List list={list}/>
+          <div key={list.id}>
+            <List list={list} text={list.text} setTodos={setTodos} output={output} />
           </div>
         );
       })}
