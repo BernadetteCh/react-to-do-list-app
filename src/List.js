537c9cc9db121c69dd3853a069755e5d04a11e07
BernadetteCh <b.christiner93@gmail.com>
2022-02-19 22:10:40 +0100

    created new function (handleCheck) to check the todo

--- src/List.js
+++ src/List.js
@@ -1,26 +1,55 @@
 import React from "react";
+import "./List.css";
 
 export default function List({ list, text, output, setTodos }) {
   function handleDelete() {
-   setTodos(output.filter((el) => el.id !== list.id));
-    //output.filter : geht durch alle todos die man bei der Eingabe erstellt. 
-    //el => el.id !== list.id : if el (element). id does not match the id of the list it gets rid of it. 
+    setTodos(output.filter((el) => el.id !== list.id));
+    //output.filter : geht durch alle todos die man bei der Eingabe erstellt.
+    //el => el.id !== list.id : if el (element). id does not match the id of the list it gets rid of it.
     //ich filtere das state in meinem fall heißt es output aus und vergleiche es mit dem was ich angeklickt habe, das ist list.id
-    //und wenn es übereinstimmt wird es gelöscht
+    //und wenn es net übereinstimmt wird es gelöscht
   }
 
-  /*  const[checked, setChecked]=useState(props.list.text);
-  
-
-  function handleCheck(event) {
-    event.preventDefault();
-    setChecked(false);*/
-
+function handleCheck() {
+  setTodos(
+    output.map(function (element) {
+      if (element.id === list.id) {
+        return { 
+          ...element,
+          completed: !element.completed,
+        };
+      }
+      return { element };
+    })
+  );
+}
+  /*
+  man könnte es auch als arrow function schreiben
+  const handleCheck = () => {
+    setTodos(
+      output.map((element) => {
+        if (element.id === list.id) {
+          return {
+            ...element,
+            completed: !element.completed,
+          };
+        }
+        return { element };
+      })
+    );
+  };
+*/
+  // basierend von dem State mappe ich durch jedes todo(was bei mir output bezeichnet wird), um Zugang zu jeden element zu haben.
+  //if ( also wenn ) von diesem die id mit der id von dem todo, welches ich gerade klicke (in diesem Fall trägt es den Namen list ), übereinstimmt
+  //gib alle elemente(meine todos) wieder und setze das completed (welches im Array als false deklariert wurde) ins Gegenteil also zu true.
+  //im Grunde vergleiche ich wieder die id und gebe es als Objekt mit properties zurück
   return (
     <div className="list">
       <fieldset>
-        <input type="checkbox" />
-        <label>{text}</label>
+        <input type="checkbox" onClick={handleCheck} />
+        <label className={`todos ${list.completed ? "completed" : ""}`}>
+          {text}
+        </label>
         <span>
           <button onClick={handleDelete}>🙉</button>
         </span>
@@ -28,4 +57,9 @@ export default function List({ list, text, output, setTodos }) {
       </fieldset>
     </div>
   );
+  //todos ist der ursprüngliche className. ich verwende `` um Javascript zu schreiben.
+  //also wenn das  todo, in diesem fall list.completed angeklickt wird, dann füge einen neuen calssName hinzu nämlich: completed
+  //ansonsten wird nichts hinzugefügt.
+  //? bedeutet if its ture und : bedeutet if not
+  //auf dem state basierend toggle ich mit diesem code completed, welches ich im CSS style
 }
