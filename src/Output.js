58ed9713a1c8ec662ad15d36fa15760b651ade51
BernadetteCh <b.christiner93@gmail.com>
2022-03-04 22:16:25 +0100

    improved loop and added key

--- src/Output.js
+++ src/Output.js
@@ -2,23 +2,24 @@ import React from "react";
 import "./Output.css";
 import List from "./List.js";
 
+export default function Output({ todos, setTodos }) {
+  //hier habe ich ein neues component (<List />) erstellt, damit ich durch loopen kann, weil jedes mal wenn
+  //i a Todo eintippe soll diese dann im output erscheinen als eigenes List component
 
-export default function Output({output,setTodos}) {
- 
-//hier habe ich ein neues component (<List />) erstellt, damit ich durch loopen kann, weil jedes mal wenn 
-//i a Todo eintippe soll diese dann im output erscheinen als eigenes List component
-return (
-  <div className="output">
-    <form>
-      {output.map(function (list) {
+  return (
+    <div>
+      {todos.map(function (todo, index) {
         return (
-          <div key={list.id}>
-            <List list={list} text={list.text} setTodos={setTodos} output={output} />
-          </div>
+          <ul key={index}>
+            <List
+              text={todo.text}
+              setTodos={setTodos}
+              todo={todo}
+              todos={todos}
+            />
+          </ul>
         );
       })}
-    </form>
-  </div>
-);
-
+    </div>
+  );
 }
