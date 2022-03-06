9292876ea98e13e2afe38c634bced3a09ecc6d46
BernadetteCh <b.christiner93@gmail.com>
2022-03-06 20:58:21 +0100

    removed old state/comment

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -5,8 +5,6 @@ import "./ToDoList.css";
 export default function ToDoList() {
   const [input, setInput] = useState(""); //dieses State sorgt dafür, dass was im input eingegeben wird dann als Todo erscheint
   const [todos, setTodos] = useState([]);
-  /*const [loaded, setLoaded] = useState(null); //dieses State sorgt dafür, dass beim Laden der Seite erstmal nur die Hauptform gezeigt wird, erst
-  //indem ich setLoaded auf true setze in der function handleSubmit, erscheint erst bei Eingabebestätigung die eingegeben Todo.*/
   const [status, setStatus] = useState("all");
   const [filteredtodos, setFilteredTodos] = useState([]); //ein zweites state mit todos wird erstellt, weil i brauch in solides array mit todos das net verändert werden soll
   //und eines wo ich die todos "herumschieben " kann
@@ -46,8 +44,7 @@ export default function ToDoList() {
       { text: input, completed: false, id: Math.random() * 1000 },
     ]); //...todos  damit sog i dem PC:wenn ich schon todos habe füge die neue Eingabe einfach hinzu
     setInput(""); //input soll leer sein sobald man eine Todo submitted hat
-    /*setLoaded(true);*/
-  };
+  }
 
   return (
     <div className="ToDoList">
