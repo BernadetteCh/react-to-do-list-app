bd020f3f442e20d13da7e8b957ae8b51290c49ce
BernadetteCh <b.christiner93@gmail.com>
2022-03-06 14:59:24 +0100

    created function filterHandler to switch the status

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -7,13 +7,28 @@ export default function ToDoList() {
   const [todos, setTodos] = useState([]);
   const [loaded, setLoaded] = useState(null); //dieses State sorgt dafür, dass beim Laden der Seite erstmal nur die Hauptform gezeigt wird, erst
   //indem ich setLoaded auf true setze in der function handleSubmit, erscheint erst bei Eingabebestätigung die eingegeben Todo.
-const[status,setStatus]=useState("all");
+  const [status, setStatus] = useState("all");
+  const [filteredtodos, setFilteredTodos] = useState([]);
+  //useEffect
 
 
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
 
-const handleStatus=(e)=>{
- setStatus(e.target.value);
-}
+  const handleStatus = (e) => {
+    setStatus(e.target.value);
+  };
 
   function handleChange(event) {
     event.preventDefault();
@@ -38,7 +53,7 @@ const handleStatus=(e)=>{
         <div className="input">
           <form onSubmit={handleSubmit}>
             <input
-            value={input}
+              value={input}
               type="text"
               placeholder="Add to list"
               className="input"
