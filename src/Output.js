8139b6d65b1f99e5d2163b273762df4790fcc351
BernadetteCh <b.christiner93@gmail.com>
2022-03-06 15:10:31 +0100

    included the filteredtodos to map through them

--- src/Output.js
+++ src/Output.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./Output.css";
 import List from "./List.js";
 
-export default function Output({ todos, setTodos }) {
+export default function Output({ todos, setTodos, filteredtodos }) {
   //hier habe ich ein neues component (<List />) erstellt, damit ich durch loopen kann, weil jedes mal wenn
   //i a Todo eintippe soll diese dann im output erscheinen als eigenes List component
 
   return (
     <div>
-      {todos.map(function (todo, index) {
+      {filteredtodos.map(function (todo, index) {
         return (
           <ul key={index}>
             <List
